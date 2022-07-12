96d06abb601c5329b3118af1dd7827dd9c099c73
Sarthak Roy <sarthakroy2002@gmail.com>
2022-07-12 09:55:45 +0530

    vendor: Copy Nothing Bootanimation for 1080p devices
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Ib509e535273892ce32978f66e56953dd356589be

--- config.mk
+++ config.mk
@@ -7,3 +7,7 @@
 PRODUCT_PACKAGES += \
 	NTSystemUI \
 	NTFrameworks
+
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+    PRODUCT_COPY_FILES += vendor/nothing/bootanimation/bootanimation1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif
