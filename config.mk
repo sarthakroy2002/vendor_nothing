d79a0468c4adac5c942dea10e2e1dfe80315af3f
Sarthak Roy <sarthakroy2002@gmail.com>
2022-07-13 11:23:54 +0530

    vendor: Import NothingOS Sounds
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I4d3fd334ca9f3cae81a876dcc42b17b289243ed3

--- config.mk
+++ config.mk
@@ -3,11 +3,18 @@
 # SPDX-License-Identifer: Apache-2.0
 #
 
-# Build support for extras
+# Overlays
 PRODUCT_PACKAGES += \
 	NTSystemUI \
 	NTFrameworks
 
+# Bootanimation
 ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
     PRODUCT_COPY_FILES += vendor/nothing/bootanimation/bootanimation1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 endif
+
+# Sounds
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/nothing/audio/alarms,$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/nothing/audio/notifications,$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/nothing/audio/ringtones,$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/nothing/audio/ui,$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui)
